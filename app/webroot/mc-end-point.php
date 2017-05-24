c0d4003fb7e755d67a36125fccfc8d80e4587ccb
Matej Latin <matejlatin@gmail.com>
2017-05-24 19:49:17 +0100

    Fixed endpoint

--- app/webroot/mc-end-point.php
+++ app/webroot/mc-end-point.php
@@ -13,47 +13,114 @@
   $email = $_GET['EMAIL']; // Get email address from form
   $id = md5(strtolower($email)); // Encrypt the email address
 
+  $status;
+  $attempt;
+  $topscore;
+
   function emailExistsMc($subscriberMail, $list_id){
     global $MailChimp;
     $subscriber_hash = $MailChimp->subscriberHash($subscriberMail);
     $result = $MailChimp->get("lists/$list_id/members/$subscriber_hash");
     // print_r($result['status']);
+    // print_r($result);
     if($result['status'] == '404'){
-      // echo "False";
       return false;
     } else {
-      // echo "True";
       return true;
     }
   }
 
+  function sendCourseMc($subscriberMail, $list_id){
+    global $MailChimp;
+    $subscriber_hash = $MailChimp->subscriberHash($subscriberMail);
+    $result = $MailChimp->get("lists/$list_id/members/$subscriber_hash");
+    // print_r($result['status']);
+    // print_r($result);
+    if($result['merge_fields']['SENDCOURSE'] != 'Yes'){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function checkAttemptMc($subscriberMail, $list_id){
+    global $MailChimp;
+    global $attempt;
+    $subscriber_hash = $MailChimp->subscriberHash($subscriberMail);
+    $result = $MailChimp->get("lists/$list_id/members/$subscriber_hash");
+    // print_r($result['status']);
+    // print_r($result);
+    if($result['merge_fields']['ATTEMPT'] < 1){
+      return false;
+    } else {
+      $attempt = $result['merge_fields']['ATTEMPT'];
+      return true;
+    }
+  }
+
+  function checkTopscoreMc($subscriberMail, $list_id){
+    global $MailChimp;
+    global $topscore;
+    $subscriber_hash = $MailChimp->subscriberHash($subscriberMail);
+    $result = $MailChimp->get("lists/$list_id/members/$subscriber_hash");
+    // print_r($result['status']);
+    // print_r($result);
+    if($result['merge_fields']['TOPSCORE'] < 1){
+      return false;
+    } else {
+      $topscore = $result['merge_fields']['TOPSCORE'];
+      return true;
+    }
+  }
+
+// emailExistsMc($email, $list);
+// sendCourseMc($email, $list);
+// checkAttemptMc($email, $list);
+
   $emailExists = emailExistsMc($email, $list);
+  $sendCourse = sendCourseMc($email, $list);
+  $checkAttempt = checkAttemptMc($email, $list);
+  $checkTopscore = checkTopscoreMc($email, $list);
 
   // setup th merge fields
-  if (emailExists) {
-    $mergeFields = array(
-      'FNAME' => $_GET['FNAME'],
-      'LNAME' => $_GET['LNAME'],
-      'ATTEMPT' => $_GET['ATTEMPT'],
-      'TOPSCORE' => $_GET['TOPSCORE'],
-      'JOINED' => $_GET['JOINED'], //remove when it works test
-    );
+  $mergeFields = array(
+    'FNAME' => $_GET['FNAME'],
+    'LNAME' => $_GET['LNAME'],
+    'PLAYEDTRI' => $_GET['PLAYEDTRI'],
+  );
+  if ($emailExists == false) {
+    $status = 'pending';
+    $mergeFields['ATTEMPT'] = $_GET['ATTEMPT'];
+    $mergeFields['TOPSCORE'] = $_GET['TOPSCORE'];
+    // $mergeFields['SENDCOURSE'] = $_GET['SENDCOURSE'];
+    $mergeFields['JOINED'] = $_GET['JOINED'];
   } else {
-    $mergeFields = array(
-      'FNAME' => $_GET['FNAME'],
-      'LNAME' => $_GET['LNAME'],
-      'ATTEMPT' => $_GET['ATTEMPT'],
-      'TOPSCORE' => $_GET['TOPSCORE'],
-      'JOINED' => $_GET['JOINED'],
-    );
+    $status = 'subscribed';
   }
+  if ($sendCourse == false) {
+    $mergeFields['SENDCOURSE'] = $_GET['SENDCOURSE'];
+  }
+  if ($checkAttempt == false) {
+    $mergeFields['ATTEMPT'] = $_GET['ATTEMPT'];
+  } else {
+    $mergeFields['ATTEMPT'] = $attempt + $_GET['ATTEMPT'];
+  }
+  if ($checkTopscore == false) {
+    $mergeFields['TOPSCORE'] = $_GET['TOPSCORE'];
+  } else {
+    if ($_GET['TOPSCORE'] > $topscore){
+      $mergeFields['TOPSCORE'] = $_GET['TOPSCORE'];
+    }
+  }
+
+  // print_r($mergeFields);
 
   // remove empty merge fields
   $mergeFields = array_filter($mergeFields);
 
   $result = $MailChimp->put("lists/$list/members/$id", array(
                   'email_address'     => $email,
-                  'status'            => 'subscribed',
+                  'status'            => $status,
                   'merge_fields'      => $mergeFields,
                   'update_existing'   => true, // YES, update old subscribers!
               ));
